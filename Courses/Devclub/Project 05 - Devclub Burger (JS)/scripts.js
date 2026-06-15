bcdb169a7811fbe6e6b4c513a10897e137907687
N9it-0 <nightisnoir0@gmail.com>
2026-06-15 15:49:21 -0300

    Project 05 - JS - ForEach, Map

--- Courses/Devclub/Project 05 - Devclub Burger (JS)/scripts.js
+++ Courses/Devclub/Project 05 - Devclub Burger (JS)/scripts.js
@@ -1,16 +1,28 @@
-const menuOptions = [
+const list = document.querySelector("ul")
+const buttonShowAll = document.getElementById("show-all")
+const buttonMapAll = document.getElementById("map-all")
 
-{ name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
+function showAll(productsArray) {
+    let myLI = ""
+    productsArray.forEach((product) => {
+        myLI += `
+        <li>
+        <img src=${product.src}>
+        <p>${product.name}</p>
+        <p class="item-price">R$ ${product.price}</p>
+        </li>
+        `
+    })
+    list.innerHTML = myLI
+}
 
-{ name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
+function mapAll() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9, //dar 10% de desconto
+    }))
+    showAll(newPrices)
+}
 
-{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
-
-{ name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
-
-{ name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
-
-{ name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-
-]
-
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonMapAll.addEventListener("click", mapAll)
